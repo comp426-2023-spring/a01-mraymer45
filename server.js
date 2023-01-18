eafbe4f325313e04799ff63469a04b6856970ae3
Mason Raymer <mcraymer@email.unc.edu>
2023-01-17 19:58:47 -0500

    Possibly final commit

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 // Require http module
 const http = require('http');
+
 // Require fs module
 const fs = require('fs');
+
 // Require minimist module (make sure you install this one via npm).
-const minimist = require('minimist');
+var argv = require('minimist')(process.argv.slice(2)); // Copied from this "site https://www.npmjs.com/package/minimist"
+
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-
+const port = argv.port;
+if (port == null) { port = "3000"; 
+}
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
+fs.readFile('./public/index.html', 'utf-8', (error, data) => {
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
-
-
-
+if (error) {
+	console.error(error);
+	return;
+}
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -28,13 +35,17 @@ const minimist = require('minimist');
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-
-
-
+const server = http.createServer((req, res) => {
+	// res.statusCode = 200;
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.end(data);
+})
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-
+server.listen(port);
+console.log(`Server listening on port ${port}');
+})
 
 
 
